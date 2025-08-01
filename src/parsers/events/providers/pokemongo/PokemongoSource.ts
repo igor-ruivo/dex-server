90a515bf1e9edcf76a19b384ce6fc5041f05557a
Igor Ruivo <igor.ruivo.97@gmail.com>
2025-08-01 22:39:19 +0100

    Include kind

--- src/parsers/events/providers/pokemongo/PokemongoSource.ts
+++ src/parsers/events/providers/pokemongo/PokemongoSource.ts
@@ -528,7 +528,18 @@ class PokemonGoSource implements IEventSource {
 				(p) => !eventData.raids.some((w) => w.speciesId === p.speciesId)
 			);
 
-			eventData.raids.push(...parsedPkm);
+			const updatedParsedPkm = parsedPkm.map((pkm) => {
+				const gmEntry = gameMasterPokemon[pkm.speciesId];
+				if (gmEntry?.isMega) {
+					return { ...pkm, kind: 'mega' };
+				}
+				if (gmEntry?.isLegendary) {
+					return { ...pkm, kind: '5' };
+				}
+				return pkm;
+			});
+
+			eventData.raids.push(...updatedParsedPkm);
 			return;
 		}
 		if (EVENT_SECTION_TYPES.INCENSE.some((x) => x === sectionType)) {
